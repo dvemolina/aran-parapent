91ff7438c08c1cf4407d61fb8fe8350ccb950264
dvemolina <dvemolina@gmail.com>
2025-02-24 11:06:31 +0000

    Adjusted size of the largescreen heading and buttons

--- src/components/HeroSection.svelte
+++ src/components/HeroSection.svelte
@@ -5,15 +5,15 @@
         <div class="content h-4/6 @5xl:h-full @5xl:w-3/5 z-10 flex flex-col justify-center items-center w-full gap-4">
             <div class="headings flex flex-col justify-center items-center w-full gap-4">
 
-                <p class="sifon text-white text-4xl @5xl:text-7xl font-bold text-center">
-                    VUELA ENTRE <br><span class="text-gradient text-outline text-[2.5rem] @5xl:text-[4.75rem]">CIMAS Y NUBES</span><br>
-                    <span class="text-xl @5xl:text-4xl">LOS PIRINEOS A TUS PIES</span>
+                <p class="sifon text-white text-4xl @5xl:text-5xl font-bold text-center">
+                    VUELA ENTRE <br><span class="text-gradient text-outline text-[2.5rem] @5xl:text-[3.25rem]">CIMAS Y NUBES</span><br>
+                    <span class="text-xl @5xl:text-3xl">LOS PIRINEOS A TUS PIES</span>
                 </p>
 
                 <p class="font-sans text-md @5xl:text-2xl text-center text-neutral-300"> Vuelos Biplaza en Parapente en Val d'Aran, <br>Ribagorza y rincones secretos del Pirineo. <br><span class="font-semibold text-neutral-200">¿Te atreves a descubrirlos?</span></p>
             </div>
                 
-            <button aria-label="Reserva Tu Vuelo" class="cta-button text-background @5xl:mt-6"><p class="text-xl @5xl:text-3xl @5xl:py-4 @5xl:px-8 text-background font-sans font-semibold">Reserva Tu Vuelo</p></button>
+            <button aria-label="Reserva Tu Vuelo" class="cta-button text-background @5xl:mt-6"><p class="text-xl @5xl:text-2xl @5xl:py-2 @5xl:px-6 text-background font-sans font-semibold">Reserva Tu Vuelo</p></button>
         </div>
     </div>
 </div>
